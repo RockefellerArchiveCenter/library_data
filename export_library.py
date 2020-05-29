61284c808e42ddd4a4cd3f094b038fb674fddd23
Amy Berish <aberish@rockarch.org>
2020-05-29 14:29:18 -0400

    reintegrate save function

--- export_library.py
+++ export_library.py
@@ -21,13 +21,13 @@ repo = aspace.repositories(2)
 
 ###Save all info (individual resources) to GitHub with file name as identifier.json
 ###will need to edit paths here
-#def save_data(object_type, object):
-#    path = os.path.join('source_data', object.jsonmodel_type) if (object_type == 'agent') else os.path.join('source_data', object_type)
-#    if not os.path.isdir(path):
-#        os.makedirs(path)
-#    with open(os.path.join(path, "{}.json".format(object.uri.split('/')[-1])), 'w+') as outfile:
-#        json.dump(object.json(), outfile, indent=2)
-#    print(os.path.join(path, "{}.json".format(object.uri.split('/')[-1])))
+def save_data(object_type, object):
+    path = os.path.join('source_data', object.jsonmodel_type) if (object_type == 'agent') else os.path.join('source_data', object_type)
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    with open(os.path.join(path, "{}.json".format(object.uri.split('/')[-1])), 'w+') as outfile:
+        json.dump(object.json(), outfile, indent=2)
+    print(os.path.join(path, "{}.json".format(object.uri.split('/')[-1])))
 
 ###get all resources that do not have AC/FA identifier
 for object_type in ['resources']:
