6f92ff9d1d6706f53e60ae8a62107728d1c5f2ef
Hillel Arnold <helrond@hotmail.com>
2020-08-18 10:15:44 -0400

    assume python 2

--- script/make_pages.py
+++ script/make_pages.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 import json
-import pathlib
+from os import listdir, mkdir, pardir
+from os.path import abspath, basename, isdir, join
 
 OBJ_PREFIX = "items"
-DATA_DIR = pathlib.Path(pathlib.Path(__file__).resolve().parents[1], "_data", "resource")
-PAGE_DIR = pathlib.Path(pathlib.Path(__file__).resolve().parents[1], OBJ_PREFIX)
+DATA_DIR = abspath(join(__file__, pardir, pardir, "_data", "resource"))
+PAGE_DIR = abspath(join(__file__, pardir, pardir, OBJ_PREFIX))
 
 
 def clean_string(string):
@@ -13,14 +14,14 @@ def clean_string(string):
 
 
 def make_pages():
-    if not PAGE_DIR.is_dir():
-        PAGE_DIR.mkdir()
-    for f in DATA_DIR.iterdir():
-        with f.open() as df:
+    if not isdir(PAGE_DIR):
+        mkdir(PAGE_DIR)
+    for f in listdir(DATA_DIR):
+        with open(join(DATA_DIR, f), "r") as df:
             data = json.load(df)
             title = clean_string(data["title"])
             obj_id = data["uri"].split("/")[-1]
-        with (PAGE_DIR / "{}.md".format(obj_id)).open(mode="w") as page:
+        with open(join(PAGE_DIR, "{}.md".format(obj_id)), "w") as page:
             page.write("---\nlayout: item\n")
             page.write("title: \"{}\" \n".format(title))
             page.write("id: {}\n".format(obj_id))
